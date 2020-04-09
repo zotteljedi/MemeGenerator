81ec97c8b13d90b3cff2667d7154065724af59d9
gregor.vonfluee <gregor.von.fluee@gmail.com>
2020-04-09 23:22:52 +0200

    load image

--- generator.py
+++ generator.py
@@ -1,4 +1,11 @@
+from PIL import Image, ImageDraw
 import argparse
+
+
+def generate_meme(image_path):
+    image = Image.open(image_path)
+    draw = ImageDraw.Draw(image)
+    image_width, image_height = image.size
 
 
 if '__main__' == __name__:
@@ -7,3 +14,5 @@ if '__main__' == __name__:
     ap.add_argument("--top", required=False, help="Top text over the image.")
     ap.add_argument("--bottom", required=False, help="Bottom text over the image.")
     args = vars(ap.parse_args())
+
+    generate_meme(args['image'])
