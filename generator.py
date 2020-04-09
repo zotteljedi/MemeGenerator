8b916aac8ce788d8163fcacbbc6eb596af917c88
gregor.vonfluee <gregor.von.fluee@gmail.com>
2020-04-09 23:23:48 +0200

    load font

--- generator.py
+++ generator.py
@@ -1,12 +1,13 @@
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 import argparse
 
 
-def generate_meme(image_path):
+def generate_meme(image_path, font_path='./res/fonts/impact.ttf', font_size=9):
     image = Image.open(image_path)
     draw = ImageDraw.Draw(image)
     image_width, image_height = image.size
 
+    font = ImageFont.truetype(font=font_path, size=int(image_height * font_size) // 100)
 
 if '__main__' == __name__:
     ap = argparse.ArgumentParser()
